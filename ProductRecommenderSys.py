663f8da45d5511d2e63ec10cb400ae608f795a8e
duytkbuikms <duytkbui@kms-technology.com>
2022-07-20 13:22:40 +0700

    explain vietnamese

--- ProductRecommenderSys.py
+++ ProductRecommenderSys.py
@@ -11,16 +11,19 @@ import Collaborative as CF
 products_data = fsData.getProductsData()
 ratings_data = fsData.getRatingData()
 
-C = products_data['ratePoint'].mean()  # C is the mean vote across the whole report.
-m = products_data['rateCount'].quantile(0.75)  # m is the minimum votes required to be listed in the chart.
+C = products_data['ratePoint'].mean()  # C la diem trung binh cong rating cua tat ca cac product
+m = products_data['rateCount'].quantile(0.75)  # m la so luong rating toi thieu cho mot san pham.
+                                                #Day la ham tinh tu phan vi, voi diem phan vi la 0.75.
+                                                # Vi the thi so luong Rating phai lon hon diem phan vi de thuat toan co the hoat dong.
 
-# Filter out all qualified products into a new DataFrame
+
+# Lọc ra tất cả các sản phẩm đủ điều kiện (co du so luong rating) vào một DataFrame mới
 q_products = products_data.copy().loc[products_data['rateCount'] >= m]
 
 
 # ================================ #
 
-# Function that computes the weighted rating of each product
+# Hàm tính toán xếp hạng trọng số của từng sản phẩm
 def weighted_rating(row, m=m, C=C):
     v = int(row['rateCount'])
     R = int(row['ratePoint'])
@@ -29,21 +32,20 @@ def weighted_rating(row, m=m, C=C):
 
 # Merge column values into one string and remove unuseful characters
 def merge_info(row):
-    baseString = str(row['name'] + ' ' + row['shopLocation'] + ' ' + row['tagName'] + '  ' + row['type'])
-    # Remove unuseful words
+    baseString = str(row['name'] + ' ' + row['shopLocation'] + ' ' + row['tagName'] + '  ' + row['type']) # chuoi String dung de so sanh cac san pham tuong tu
+    # Ham dung de xoa cac tu khong can thiet
     # unusefulWords = ['·', 'Vietnam', 'Việt Nam', ',', 'Phòng ', 'phòng ', '-']
     unusefulWords = []
+
     big_regex = re.compile('|'.join(map(re.escape, unusefulWords)))
     usefulString = big_regex.sub('', baseString)
-    # Combine number and word into one word
+    # Kết hợp số và từ thành một từ
     result = re.sub('(?<=\d) (?=\w)', '', usefulString)
     return result
 
 
 # ========== Constant ============ #
-TEST_PERCENT = 0.2  # test 20% of data
-K_USER = 100
-UUCF = 1
+
 
 
 class ProductRecommenderSys(object):
@@ -51,78 +53,62 @@ class ProductRecommenderSys(object):
         self.dfProducts = products_data
         self.dfRatings = ratings_data
 
-    # Get recommend products based on WEIGHTED RATING
+    # Nhận đề xuất các sản phẩm dựa trên WEIGHTED RATING
     def get_weighted_rating(self):
-        # Define a new feature 'score' and calculate its value with `weighted_rating()`
+        # Tao field moi cho q_products la field score và tính giá trị của nó với `weighted_rating () '
         q_products['score'] = q_products.apply(weighted_rating, axis=1)
 
-        # Sort products based on score calculated above
+        # Sắp xếp sản phẩm dựa trên số điểm đã tính ở trên
         result = q_products.sort_values('score', ascending=False)
 
-        # Get top 6 highest score
+        # Lay top 6 san pham dạt điểm cao nhất
         return result['id'][:7].to_list()
 
-    # Get recommend products based on Content-based Filtering
+    # Lay đề xuất sản phẩm dựa trên Content-based Filtering
     def get_content_based(self, id):
         df = self.dfProducts
-        # Filter out all qualified products based on city into a new DataFrame
+        # Lọc ra tất cả các sản phẩm đủ điều kiện dựa trên tagName vào một DataFrame mới
         row_index_matchId = df.index[df['id'] == id].tolist()
         df = df.copy().loc[df['tagName'] == df['tagName'].values[row_index_matchId[0]]]
 
-        # Construct a reverse map of indices and idProduct
+        # Xây dựng map dao ngược của các chỉ số và idProduct
         indices = pd.Series(df.index, index=df['id']).drop_duplicates()
 
-        # Get the index of the product that matches the idProduct
+        # Lay vi tri của sản phẩm trong map o tren phù hợp với idProduct
         idx = indices[id]
 
-        # Columns
+        # Columns chua cac field can so sanh
         columns = ['name', 'shopLocation', 'tagName', 'type']
 
-        # Convert column values to String
+        # Chuyển đổi giá trị cột thành String idProduct
         for column in columns:
             df[column] = df[column].apply(str)
 
-        # Create new feature mergeInfo by apply merge_info method
+        # Tạo field moi la mergeInfo bằng cách áp dụng phương thức merge_info
         df['mergeInfo'] = df.apply(merge_info, axis=1)
 
-        # Define a TF-IDF Vectorizer Object.
+        # Define bien tfidf la mot ma tran vector TF-IDF
         tfidf = TfidfVectorizer()
-        # Construct the required TF-IDF matrix by fitting and transforming the data
+        # Xây dựng ma trận TF-IDF cần thiết bằng cách điều chỉnh và biến đổi dữ liệu
         tfidf_matrix = tfidf.fit_transform(df['mergeInfo'])
 
-        # Compute the cosine similarity matrix
+        # Tính toán ma trận bang phuong phap cosine_similarity
         cosine_sim_matrix = cosine_similarity(tfidf_matrix, tfidf_matrix)
 
-        # Get the pairwsie similarity scores of all products with that product
+        # Get cac cap co diem giong tuong tu voi san pham dang xet voi tong cac san pham cung tagName trong DB
         sim_scores = list(enumerate(cosine_sim_matrix[idx]))
 
-        # Sort the products based on the similarity scores
+        # Sắp xếp các sản phẩm dựa trên điểm giống nhau
         sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
 
-        # Get the scores of the 6 most similar products
+        # Lay điểm của 6 sản phẩm giống nhất
         sim_scores = sim_scores[1:7]
 
-        # Get the product indices
+        # Lay cac vi tri cua san pham
         products_indices = [i[0] for i in sim_scores]
 
         result = df['id'].iloc[products_indices]
 
-        # Return the top 6 most similar products
+        # Trở ve top 6 sản phẩm tương tự nhất
         return result.to_list()
 
-    # Get recommend products based on Collaborative Filtering
-    # def get_collaborative(self, userId):
-    #     # prepare data
-    #     df = self.dfRatings
-    #     dfRating = df.filter(items=['userId', 'hotelId', 'ratingPoint'])
-    #     rate_train, rate_test = train_test_split(dfRating, test_size=TEST_PERCENT)
-
-    #     # Training data
-    #     Y_data = rate_train.to_numpy()
-    #     rs = CF.Collaborative(Y_data, k=K_USER, uuCF=1)
-    #     rs.training()
-
-    #     # RMSE on test data
-    #     print('RMSE =', rs.getRMSE(rate_test))
-
-    #     return rs.get_recommendation(userId)
